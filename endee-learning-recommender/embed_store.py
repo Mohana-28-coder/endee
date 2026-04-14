ef4e38c93c42f91722def8abe940564c7ae6027d
Mohanapriya Chandrasekaran <priyanandhinic2005@gmail.com>
2026-04-14 20:22:03 +0530

    Update embed_store.py

--- endee-learning-recommender/embed_store.py
+++ endee-learning-recommender/embed_store.py
@@ -6,22 +6,18 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 topics = open("data.txt").read().splitlines()
 
-# Create embeddings
 embeddings = model.encode(topics)
-
-# Convert to numpy
 embeddings = np.array(embeddings).astype("float32")
 
-# Create FAISS index
 dimension = embeddings.shape[1]
+
 index = faiss.IndexFlatL2(dimension)
 index.add(embeddings)
 
-# Save data
 faiss.write_index(index, "topics.index")
 
 with open("topics.txt", "w") as f:
     for t in topics:
         f.write(t + "\n")
 
-print("✅ FAISS vector DB created successfully")
+print("✅ FAISS DB created")
